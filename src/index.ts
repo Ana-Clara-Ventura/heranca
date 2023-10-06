18898dcbb2d3dc278cf84b9b88ee4ccb1ece9df0
Ana-Clara-Ventura <126262513+Ana-Clara-Ventura@users.noreply.github.com>
2023-10-06 15:23:28 -0300

    Arrumar espaçamento

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { Comissionado } from "./model/Comissionado";
 import { Horista } from "./model/Horista";
 
 const a1 = new Assalariado(`Miroslav Klose`, `201420142014`, 1500);
-
 a1.setNome("Ana Clara");
 a1.setCpf("102912932819");
 a1.setSalario(2000);
